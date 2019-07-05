bd9186838d0dffc7e81056d90a3bf66de9a2f803
Hajime Kawahara <divrot@gmail.com>
2019-07-05 19:05:37 +0900

    spots fig

--- lat/latsb9.py
+++ lat/latsb9.py
@@ -1,8 +1,34 @@
 import pandas as pd
 import numpy as np
 import typetomass as ttm
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy import units as u
+from astropy.time import Time
+from astropy.coordinates import get_sun
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+
+    thetamin=100.0 #mas
+    thetamax=1000.0 #mas
+    maxalt = 30.0 #maximum altitude
+    lat_subaru=19.0 + 49/60.+ 43/3600.0
+    lon_subaru=155.0 + 28/60.0 + 50/3600.0 
+    height_subaru = 4139.0
+    utcoffset = - 10.0*u.hour
+    midlocal=Time('2019-7-16 00:00:00')
+    midnight = midlocal + utcoffset
+    print(midlocal.iso)
+    location_subaru=EarthLocation(lat=lat_subaru*u.deg, lon=lon_subaru*u.deg, height=height_subaru*u.m)
+
+    delta_midnight = np.linspace(-12, 12, 1000)*u.hour
+    times_obs = midnight + delta_midnight
+    frame=AltAz(obstime=midnight+delta_midnight,location=location_subaru)
+    sunaltazs = get_sun(midnight+delta_midnight).transform_to(frame)
+    nightmask=(sunaltazs.alt < -0*u.deg)
+
+
+    
     dat=pd.read_csv("../database/sb9/sb9.csv")
     dbV=pd.read_csv("../database/sp/spV.txt",delimiter=",")
 #    print(dbV["mass"][dbV["Sp"]=="O8"].values[0])
@@ -24,7 +50,8 @@ if __name__ == "__main__":
         #print("Mtot=",totalmass,"P=",P,"(d) a=",a,"au")
         #print("d=",1/(plx_simbad/1000),"pc")
         #print("theta gaia=",theta_gaia,"mas","theta simbad=",theta_simbad,"mas")
-        if theta_simbad > 100:
+        
+        if theta_simbad > thetamin and theta < thetamax:
             print(sp1,mass1,sp2,mass2,"theta simbad=",theta_simbad,"mas",K1,K2)
 #            print("Mtot=",totalmass,"P=",P,"(d) a=",a,"au")
 #            print("d=",1/(plx_simbad/1000),"pc")
--- lat/latspots.py
+++ lat/latspots.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 import typetomass as ttm
-
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy import units as u
+from astropy.time import Time
+from astropy.coordinates import get_sun
+import matplotlib.pyplot as plt
 #Seq|Star|RAJ2000|DEJ2000|Dist|n_Dist|Age|Hmag|SpType|MA|l_MB|MB|l_rho|rho|l_Per|Per|x_Per|l_Ecc|Ecc|l_acrit|acrit|Ref|SimbadName
 
 def is_float_expression(s):
@@ -12,7 +16,32 @@ def is_float_expression(s):
         return False
     
 if __name__ == "__main__":
+
+    thetamin=100.0 #mas
+    thetamax=1000.0 #mas
+    maxalt = 30.0 #maximum altitude
+    lat_subaru=19.0 + 49/60.+ 43/3600.0
+    lon_subaru=155.0 + 28/60.0 + 50/3600.0 
+    height_subaru = 4139.0
+    utcoffset = - 10.0*u.hour
+    midlocal=Time('2019-7-16 00:00:00')
+    midnight = midlocal + utcoffset
+    print(midlocal.iso)
+    location_subaru=EarthLocation(lat=lat_subaru*u.deg, lon=lon_subaru*u.deg, height=height_subaru*u.m)
+
+    delta_midnight = np.linspace(-12, 12, 1000)*u.hour
+    times_obs = midnight + delta_midnight
+    frame=AltAz(obstime=midnight+delta_midnight,location=location_subaru)
+    sunaltazs = get_sun(midnight+delta_midnight).transform_to(frame)
+    nightmask=(sunaltazs.alt < -0*u.deg)
+
+    
     dat=pd.read_csv("../database/spots/spots.tsv",delimiter="|")
+
+    fig=plt.figure(figsize=(15,7))
+    ax=fig.add_subplot(121)
+    ic=0
+    lsarr=["solid","dashed","dotted"]
     for i,name in enumerate(dat["Star"]):
         d=float(dat["Dist"][i])
         sp=dat["SpType"][i]
@@ -37,5 +66,33 @@ if __name__ == "__main__":
                 theta=a/d*1000
             except:
                 theta=-1.0
-        if theta > 100.0:
+                
+
+        
+        #print(name,c.to_string('hmsdms'))
+        if theta > thetamin and theta < thetamax:
             print(name,sp,mass1,mass2,"theta=",theta)
+            ra=dat["RAJ2000"][i]
+            dec=dat["DEJ2000"][i]
+            c = SkyCoord(ra, dec, unit=(u.hourangle, u.deg))
+
+            altitude = c.transform_to(frame)
+            if np.max(altitude.alt[nightmask]) > maxalt*u.deg:
+                iic=np.mod(ic,7)
+                lab=name+", "+sp+", mA="+str(mass1)+", mB="+str(mass2)+", $\\theta$= "+str(round(theta,1))+" mas,"+" H="+str(round(float(dat["Hmag"][i]),1))
+                plt.plot(delta_midnight,altitude.alt,label=lab,color="C"+str(iic),ls=lsarr[int(ic/7)])
+                ic=ic+1
+                
+    plt.fill_between(delta_midnight.to('hr').value, 0, 90,
+                 sunaltazs.alt < -0*u.deg, color='0.5', zorder=0)
+    plt.fill_between(delta_midnight.to('hr').value, 0, 90,
+                 sunaltazs.alt < -18*u.deg, color='k', zorder=0)
+    plt.ylim(0.,90)
+    plt.xlim(-12, 12)
+    plt.xticks(np.arange(13)*2 -12)
+    plt.ylim(0, 90)
+    plt.xlabel('Hours from Midnight = '+midlocal.iso)
+    plt.ylabel('Altitude [deg]')
+    plt.title("SPOTS")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=10)
+    plt.show()
